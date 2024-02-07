de3e02e951c866cdb4828e23c82ecfaf5a91e0c1
nu12 <34694287+nu12@users.noreply.github.com>
2024-02-07 13:27:51 -0500

    fix: create pkg folder

--- pkg/rand.go
+++ pkg/rand.go
@@ -1,6 +1,6 @@
-package main
+package rand
 
-import rand "github.com/nu12/rand/internal"
+import internal "github.com/nu12/rand/internal"
 
 // Alpha returns a string of random characters and numbers of a given length.
 //
@@ -8,7 +8,7 @@ import rand "github.com/nu12/rand/internal"
 //
 // lowerOnly and upperOnly define whether only lower case or upper case characters are present in the output.
 func Alpha(lenght int, lowerOnly, upperOnly bool) string {
-	return rand.GenerateAlpha(lenght, lowerOnly, upperOnly)
+	return internal.GenerateAlpha(lenght, lowerOnly, upperOnly)
 }
 
 // Char returns a string of random characters of a given length.
@@ -17,14 +17,14 @@ func Alpha(lenght int, lowerOnly, upperOnly bool) string {
 //
 // lowerOnly and upperOnly define whether only lower case or upper case characters are present in the output.
 func Char(lenght int, lowerOnly, upperOnly bool) string {
-	return rand.GenerateChar(lenght, lowerOnly, upperOnly)
+	return internal.GenerateChar(lenght, lowerOnly, upperOnly)
 }
 
 // Num returns a string of random numbers of a given length.
 //
 // length defines the length of the output string.
 func Num(lenght int) string {
-	return rand.GenerateNum(lenght)
+	return internal.GenerateNum(lenght)
 }
 
 // Password returns a string of random characters, numbers and special signs of a given length.
@@ -33,17 +33,17 @@ func Num(lenght int) string {
 //
 // lowerOnly and upperOnly define whether only lower case or upper case characters are present in the output.
 func Password(lenght int, lowerOnly, upperOnly bool) string {
-	return rand.GeneratePassword(lenght, lowerOnly, upperOnly)
+	return internal.GeneratePassword(lenght, lowerOnly, upperOnly)
 }
 
 // Special returns a string of random special characters of a given length.
 //
 // length defines the length of the output string.
 func Special(lenght int) string {
-	return rand.GenerateSpecial(lenght)
+	return internal.GenerateSpecial(lenght)
 }
 
 // UUID returns an UUID
 func UUID() string {
-	return rand.GenerateUUID()
+	return internal.GenerateUUID()
 }
